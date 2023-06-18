402105a259940f1aa32b5dfc48b7f0e7a5022c81
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-18 11:49:01 -0400

    moved Constructbuilding<T>(...) from Building.cs to ConstructionLogic.cs.

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Building.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Building.cs
@@ -11,31 +11,5 @@ namespace Software2KnowledgeCheck1
         public string Street { get; set; }
 
         public int StreetNumber { get; set; }
-
-        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
-        {
-            if (permit && zoning)
-            {
-                foreach (var material in materials)
-                {
-                    if (material == "concrete")
-                    {
-                        // start laying foundation
-                    }
-                    else if (material == "Steel")
-                    {
-                        // Start building structure
-                    }
-                    // etc etc...
-
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
     }
 }

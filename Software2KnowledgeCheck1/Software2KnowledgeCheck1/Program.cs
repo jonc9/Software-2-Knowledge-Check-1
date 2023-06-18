96a0d6fbf6b8d1db1d31abe2724f4aea07734a95
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-18 12:36:34 -0400

    Added code if I were to try and actually run this console application.

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Program.cs
@@ -1,7 +1,17 @@
-﻿public class Program
+﻿using Software2KnowledgeCheck1;
+
+public class Program
 {
     static void Main(string[] args)
     {
+        var constructionLogic = new ConstructionLogic();
+        var apartment = new Apartment();
+        var highRise = new HighRise();
+        var building = new Building();
 
+        var buildings = new List<Building>() { highRise, building};
+
+
+        constructionLogic.CreateBuilding(apartment, buildings);
     }
 }

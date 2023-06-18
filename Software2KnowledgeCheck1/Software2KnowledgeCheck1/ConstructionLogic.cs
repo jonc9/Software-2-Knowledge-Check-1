dc9b3bed5e892c7c57ec42622e87fe49d51a0c44
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-18 11:49:56 -0400

    Move CreateApartment(...) & ConstructBuilding<T>(...) into this class from Apartments.cs and Building.cs respectively.

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/ConstructionLogic.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/ConstructionLogic.cs
@@ -8,5 +8,45 @@ namespace Software2KnowledgeCheck1
 {
     internal class ConstructionLogic
     {
+        public void CreateApartment(Apartment apartment, List<Building> buildings)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                buildings.Add(apartment);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }

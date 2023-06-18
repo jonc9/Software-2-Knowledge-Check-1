12fca33cd3a4cbccfdaf16445c76f23a0dc3f481
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-18 17:05:34 -0400

    Removed some excess whitespace.

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Apartment.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Apartment.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace Software2KnowledgeCheck1
 {
-    internal class Apartment : Building
+    internal class Apartment : BuildingLocation
     {
         public int NumberOfUnits { get; set; }
         public int NumberOfOpenUnits { get; set; }
-
         public bool HasParking { get; set; }
         public string? OwnerName { get; set; } // moved from HighRise.cs
         public List<string> Directory { get; set; } // moved from HighRise.cs
-
-        public List<Building> Buildings { get; } = new List<Building>();
-
-        
+        public List<BuildingLocation> Buildings { get; } = new List<BuildingLocation>(); 
     }
 }

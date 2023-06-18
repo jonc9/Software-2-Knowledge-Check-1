1074d1ac0177388f8c6f13eceb92683b2b546a00
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-18 17:08:15 -0400

    Moved OwnerName & List<string> Directory to Apartments.cs. I left a comment explaining too.

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/HighRise.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/HighRise.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace Software2KnowledgeCheck1
 {
-    internal class HighRise : Apartment
+    internal class HighRise : Apartment /*Originally inherited Building but upon looking at SOLID principles, I felt Apartment.cs would be better due to it inheriting from building and since a highrise is an apartment, its just dependent on how many feet above ground level. I also moved OwnerName to Apartment due to them being so similar and moved "List<string> Directory" to apartment as well. */
+       
     {
         public int NumberOfStories { get; set; }
-        public string OwnerName { get; set; }
-        public List<string> Directory  { get; set; }
     }
 }
